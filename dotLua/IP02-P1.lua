e2886a181e54b2ff576f7c5a9443ae866bd69970
kaissadine <141329977+kaissadine@users.noreply.github.com>
2023-11-14 18:13:15 -0800

    Create IP02-P1.lua

---
+++ dotLua/IP02-P1.lua
@@ -0,0 +1,15 @@
+local a = 12+3/2
+local b = (12+3)/2
+local c = 45/2
+local d = 45%2
+local e = 45.0/2.0
+local f = (12+3)*10/5
+local g = 12+3*10/5
+
+print("the first question was: 12+3/2 and the result is:",a)
+print("the second question was: (12+3)/2 and the result is:",b)
+print("the third question was: 45/2 and the result is:",c)
+print("the fourth question was: 45%2 and the result is:",d)
+print("the fifth question was: 45.0/2.0 and the result is:",e)
+print("the sixth question was: (12+3)*10/5 and the result is:",f)
+print("the seventh question was: 12+3*10/5 and the result is:",g)
