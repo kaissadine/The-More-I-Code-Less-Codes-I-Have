59f4f5bd18123b8e745dd5f4f75ac7a09ea8b34d
kaissadine <141329977+kaissadine@users.noreply.github.com>
2024-02-19 14:52:35 -0300

    Create 19-02-2024-EX1.c

---
+++ dotC/19-02-2024-EX1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+    int number, somam = 0, kaunta = 0, op;
+    float numberf, somamf = 0;
+    printf("type 0 for int and 1 for float: ");
+    scanf("%d", &op);
+    system("cls");
+    switch (op)
+    {
+    case 0:
+        do
+        {
+            printf("type a int number from 0 to 10: ");
+            scanf("%d", &number);
+            somam += number;
+            system("cls");
+            kaunta++;
+        } while (number != 0);
+        kaunta--;
+        printf("you typed %d numbers and the average number of they is = %d", kaunta, somam);
+        break;
+    case 1:
+        do
+        {
+            printf("type a float number from 0 to 10: ");
+            scanf("%f", &numberf);
+            somamf += numberf;
+            system("cls");
+            kaunta++;
+        } while (numberf != 0);
+        kaunta--;
+        somamf=somamf/kaunta;
+        printf("you typed %d numbers and the average number of they is = %.2f", kaunta, somamf);
+        break;
+    default:
+        printf("wrong answer");
+    }
+}
